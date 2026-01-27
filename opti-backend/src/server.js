a26f87d4caf7b9cdcf65e36467da7adf410fec92
siddarthzodopt <siddarth@zodopt.com>
2026-01-27 10:06:51 +0530

    Update server.js

--- opti-backend/src/server.js
+++ opti-backend/src/server.js
@@ -84,6 +84,7 @@ app.use((err, req, res, next) => {
 /* ===============================
    START SERVER
 ================================ */
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
