7d553717a674a74129b1f1a2375c5554888cbaae
siddarthzodopt <siddarth@zodopt.com>
2026-01-27 22:06:33 +0530

    Create validators.js

---
+++ opti-backend/src/utils/validators.js
@@ -0,0 +1,56 @@
+/**
+ * Check if password meets strength requirements
+ * @param {string} password - Password to validate
+ * @returns {boolean} True if password is strong
+ */
+const isStrongPassword = (password) => {
+  if (!password || password.length < 8) return false;
+  
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumbers = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+  
+  return hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+};
+
+/**
+ * Get detailed password validation errors
+ * @param {string} password - Password to validate
+ * @returns {array} Array of error messages
+ */
+const getPasswordErrors = (password) => {
+  const errors = [];
+  
+  if (!password) {
+    errors.push('Password is required');
+    return errors;
+  }
+  
+  if (password.length < 8) {
+    errors.push('Password must be at least 8 characters long');
+  }
+  
+  if (!/[A-Z]/.test(password)) {
+    errors.push('Password must contain at least one uppercase letter');
+  }
+  
+  if (!/[a-z]/.test(password)) {
+    errors.push('Password must contain at least one lowercase letter');
+  }
+  
+  if (!/\d/.test(password)) {
+    errors.push('Password must contain at least one number');
+  }
+  
+  if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+    errors.push('Password must contain at least one special character');
+  }
+  
+  return errors;
+};
+
+module.exports = {
+  isStrongPassword,
+  getPasswordErrors,
+};
